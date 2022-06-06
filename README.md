ef5360233fe19f9b77127bfbffcbae8cb9dad200
Vonr <dtaclanlegion@gmail.com>
2022-06-06 08:26:22 +0800

    Leftward alignment and n-character alignment

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### align.nvim is a minimal plugin for NeoVim for aligning lines
 
-align.nvim supports aligning lines to the rightmost character, string, or Lua pattern.
+align.nvim supports aligning lines to the mmost feasible leftward or rightmost character, string, or Lua pattern.
 
 Escapes for Lua patterns can either be % or \\, the latter both of which can be escaped again with another \\.
 
@@ -21,9 +21,14 @@ Example:
 
 ```lua
 local NS = { noremap = true, silent = true }
-vim.keymap.set('v', 'aa', function() require'align'.align_to_char()       end, NS) -- Align to character
-vim.keymap.set('v', 'aw', function() require'align'.align_to_string()     end, NS) -- Align to word
-vim.keymap.set('v', 'ar', function() require'align'.align_to_string(true) end, NS) -- Align to pattern
+
+vim.keymap.set('v', 'aa', function() require'align'.align_to_char()              end, NS) -- Align to rightmost character
+vim.keymap.set('v', 'aq', function() require'align'.align_to_char(2)             end, NS) -- Align to rightmost 2 characters
+vim.keymap.set('v', 'ac', function() require'align'.align_to_char(1, true)       end, NS) -- Align to most feasible leftward character
+vim.keymap.set('v', 'aw', function() require'align'.align_to_string()            end, NS) -- Align to rightmost string
+vim.keymap.set('v', 'as', function() require'align'.align_to_string(false, true) end, NS) -- Align to most feasible leftward character
+vim.keymap.set('v', 'ar', function() require'align'.align_to_string(true)        end, NS) -- Align to rightmost pattern
+vim.keymap.set('v', 'ae', function() require'align'.align_to_string(true, true)  end, NS) -- Align to most feasible leftward pattern
 ```
 
 ![Usage Gif](align.gif)
--- lua/align/init.lua
+++ lua/align/init.lua
@@ -1,7 +1,8 @@
-local function align(str, is_pattern)
+local function align(str, is_pattern, reverse)
     if not str or str == '' then
         return
     end
+    reverse = not not reverse
 
     local _, sr, sc, _ = unpack(vim.fn.getpos('v'))
     local _, er, ec, _ = unpack(vim.fn.getcurpos())
@@ -9,7 +10,7 @@ local function align(str, is_pattern)
         return
     end
 
-    local max_col = 0
+    local target = 0
     local positions = {}
 
     if not is_pattern then
@@ -44,39 +45,77 @@ local function align(str, is_pattern)
             if i == er and ec < start and visual then
                 break
             end
-            max_col = math.max(max_col, start)
             table.insert(positions, { i, start })
+            if reverse then
+                print('Hi')
+                for j = start - 1, 1, -1 do
+                    if line:sub(j, j) == " " then
+                        start = j
+                        Start = start
+                    else
+                        break
+                    end
+                end
+            end
+            target = math.max(target, start)
         end
+        Target = target
     end
 
-    if max_col == 0 or #positions == 0 then
+    if target == 0 or #positions == 0 then
         return
     end
 
     for _, pos in ipairs(positions) do
         local r, c = unpack(pos)
         local curr = vim.api.nvim_buf_get_lines(0, r - 1, r, true)[1]
-        vim.api.nvim_buf_set_lines(
-            0    ,
-            r - 1,
-            r    ,
-            true ,
-            { string.insert(curr, c, (' '):rep(max_col - c)) }
-        )
+        -- vim.api.nvim_buf_set_lines(
+        --     0    ,
+        --     r - 1,
+        --     r    ,
+        --     true ,
+        --     { string.insert(curr, c, (' '):rep(target - c)) }
+        -- )
+        if c <= target then
+            vim.api.nvim_buf_set_lines(
+                0    ,
+                r - 1,
+                r    ,
+                true ,
+                { string.insert(curr, c, (' '):rep(target - c + (reverse and 1 or 0))) }
+            )
+        else
+            -- Use string.sub to split around spaces
+            vim.api.nvim_buf_set_lines(
+                0    ,
+                r - 1,
+                r    ,
+                true ,
+                { string.sub(curr, 1, target) .. string.sub(curr, c) }
+            )
+        end
     end
     vim.api.nvim_input('<Esc>')
 end
 
-local function align_to_char()
-    print('Enter a character: ')
-    local ch = vim.fn.getcharstr()
-    print('Enter a character: ' .. ch)
-    align(ch)
+local function align_to_char(number, reverse)
+    number = number or 1
+    if number < 1 then
+        return
+    end
+    local prompt = 'Enter ' .. (number > 1 and number .. ' characters: ' or 'a character: ')
+    print(prompt)
+    local str = ''
+    for _ = 1, number do
+        str = str .. vim.fn.getcharstr()
+        print(prompt .. str)
+    end
+    align(str, false, not not reverse)
 end
 
-local function align_to_string(is_pattern)
+local function align_to_string(is_pattern, reverse)
     local str = vim.fn.input(is_pattern and 'Enter pattern: ' or 'Enter string: ')
-    align(str, not not is_pattern)
+    align(str, not not is_pattern, not not reverse)
 end
 
 return {
